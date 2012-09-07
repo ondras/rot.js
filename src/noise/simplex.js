53621aa3a547b659509147b1651e6579946364ba
Ondrej Zara <ondrej.zara@gmail.com>
2012-09-07 16:08:53 +0200

    more noise

--- src/noise/simplex.js
+++ src/noise/simplex.js
@@ -9,14 +9,13 @@
 
 /**
  * @class 2D simplex noise generator
- * @param {int} [gridSize=16] Grid size
+ * @param {int} [gradients=256] Random gradients
  */
-ROT.Noise.Simplex = function(gridSize) {
+ROT.Noise.Simplex = function(gradients) {
 	ROT.Noise.call(this);
 
-	this._F2 = 0.5 * (Math.sqrt(3.0) - 1.0);
-    this._G2 = (3.0 - Math.sqrt(3.0)) / 6.0;
-	this._gridSize = gridSize || 16;
+	this._F2 = 0.5 * (Math.sqrt(3) - 1);
+    this._G2 = (3 - Math.sqrt(3)) / 6;
 
 	this._gradients = [
 		[ 0, -1],
@@ -29,20 +28,26 @@ ROT.Noise.Simplex = function(gridSize) {
 		[-1, -1]
 	];
 
-	this._grid = [];
-	for (var i=0;i<this._gridSize;i++) {
-		this._grid.push([]);
-		for (var j=0;j<this._gridSize;j++) {
-			var index = ~~(ROT.RNG.getUniform()*this._gradients.length);
-			this._grid[i].push(index);
-		}
+	var permutations = [];
+	var count = gradients || 256;
+	for (var i=0;i<count;i++) { permutations.push(i); }
+	permutations = permutations.randomize();
+
+	this._perms = [];
+	this._indexes = [];
+
+	for (var i=0;i<2*count;i++) {
+		this._perms.push(permutations[i % count]);
+		this._indexes.push(this._perms[i] % this._gradients.length);
 	}
+
 };
 ROT.Noise.Simplex.extend(ROT.Noise);
 
 ROT.Noise.Simplex.prototype.get = function(xin, yin) {
-	xin *= this._gridSize;
-	yin *= this._gridSize;
+	var perms = this._perms;
+	var indexes = this._indexes;
+	var count = perms.length/2;
 
 	var n0 =0, n1 = 0, n2 = 0; // Noise contributions from the three corners
 	// Skew the input space to determine which simplex cell we're in
@@ -70,38 +75,36 @@ ROT.Noise.Simplex.prototype.get = function(xin, yin) {
 	// c = (3-sqrt(3))/6
 	var x1 = x0 - i1 + this._G2; // Offsets for middle corner in (x,y) unskewed coords
 	var y1 = y0 - j1 + this._G2;
-	var x2 = x0 - 1.0 + 2.0 * this._G2; // Offsets for last corner in (x,y) unskewed coords
-	var y2 = y0 - 1.0 + 2.0 * this._G2;
+	var x2 = x0 - 1 + 2 * this._G2; // Offsets for last corner in (x,y) unskewed coords
+	var y2 = y0 - 1 + 2 * this._G2;
 	// Work out the hashed gradient indices of the three simplex corners
+	var ii = i % count;
+	var jj = j % count;
+	var gi0 = indexes[ii+perms[jj]];
+	var gi1 = indexes[ii+i1+perms[jj+j1]];
+	var gi2 = indexes[ii+1+perms[jj+1]];
 
 
 	// Calculate the contribution from the three corners
 	var t0 = 0.5 - x0 * x0 - y0 * y0;
 	if (t0 >= 0) {
 		t0 *= t0;
-
-		var index = this._grid[i % this._gridSize][j % this._gridSize];
-		var grad = this._gradients[index];
+		var grad = this._gradients[gi0];
 		n0 = t0 * t0 * (grad[0] * x0 + grad[1] * y0);
 	}
 	var t1 = 0.5 - x1 * x1 - y1 * y1;
 	if (t1 >= 0) {
 		t1 *= t1;
-
-		var index = this._grid[(i+i1) % this._gridSize][(j+j1) % this._gridSize];
-		var grad = this._gradients[index];
+		var grad = this._gradients[gi1];
 		n1 = t1 * t1 * (grad[0] * x1 + grad[1] * y1);
 	}
 	var t2 = 0.5 - x2 * x2 - y2 * y2;
 	if (t2 >= 0) {
 		t2 *= t2;
-
-		var index = this._grid[(i+1) % this._gridSize][(j+1) % this._gridSize];
-		var grad = this._gradients[index];
-
+		var grad = this._gradients[gi2];
 		n2 = t2 * t2 * (grad[0] * x2 + grad[1] * y2);
 	}
 	// Add contributions from each corner to get the final noise value.
 	// The result is scaled to return values in the interval [-1,1].
-	return 70.0 * (n0 + n1 + n2);
+	return 70 * (n0 + n1 + n2);
 }
