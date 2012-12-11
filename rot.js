02f29fbcfbca9f2ee5b710d63b562019542c87c7
Kim Horne <kim@smarter-apps.com>
2012-12-10 20:05:31 -0500

    Initial layer work
    Rudimentary layer support.  Drawing text works in the basic case but
    doesn't necessarily respect redrawing across layers.  Ie: if I clear a
    portion of the top region then corresponding areas of the regions below
    are not redrawn.

--- rot.js
+++ rot.js
@@ -1,6 +1,6 @@
 /*
 	This is rot.js, the ROguelike Toolkit in JavaScript.
-	Version 0.3~dev, generated on Wed Dec  5 10:11:14 CET 2012.
+	Version 0.3~dev, generated on Mon 10 Dec 2012 20:02:38 EST.
 */
 
 /**
@@ -62,8 +62,8 @@ var ROT = {
 ROT.Display = function(options) {
 	this._canvas = document.createElement("canvas");
 	this._context = this._canvas.getContext("2d");
-	this._data = {};
-	this._dirty = false; /* false = nothing, true = all, object = dirty cells */
+	this._dirty = false;
+	this._layers = [];
 	this._charWidth = 0;
 	this._hexSize = 0;
 	this._hexSpacingX = 0;
@@ -86,6 +86,7 @@ ROT.Display = function(options) {
 	this.setOptions(defaultOptions);
 	this.DEBUG = this.DEBUG.bind(this);
 	
+	this._background = this.newLayer();
 	this._interval = setInterval(this._tick.bind(this), 1000/this._options.fps);
 }
 
@@ -101,11 +102,22 @@ ROT.Display.prototype.DEBUG = function(x, y, what) {
 }
 
 /**
- * Clear the whole display (cover it with background color)
+ * Clear the whole display (cover it with background color).  This applies to the background layer.
  */
 ROT.Display.prototype.clear = function() {
-	this._data = {};
-	this._dirty = true;
+	this._background.clear();
+}
+
+/**
+ * Clear a specified region of the display. This applies to the background layer.
+ * 
+ * @param {int} x
+ * @param {int} y
+ * @param {int} w
+ * @param {int} h
+ */
+ROT.Display.prototype.clearRect = function(x, y, w, h) {
+	this._background.clearRect(x, y, w, h);
 }
 
 /**
@@ -137,6 +149,7 @@ ROT.Display.prototype.getContainer = function() {
 }
 
 /**
+ * This applies to the background layer.
  * @param {int} x
  * @param {int} y
  * @param {string} ch 
@@ -144,17 +157,11 @@ ROT.Display.prototype.getContainer = function() {
  * @param {string} [bg] background color
  */
 ROT.Display.prototype.draw = function(x, y, ch, fg, bg) {
-	if (!fg) { fg = this._options.fg; }
-	if (!bg) { bg = this._options.bg; }
-	this._data[x+","+y] = [x, y, ch, fg, bg];
-	
-	if (this._dirty === true) { return; } /* will already redraw everything */
-	if (!this._dirty) { this._dirty = {}; } /* first! */
-	this._dirty[x+","+y] = true;
+	this._background.draw(x, y, ch, fg, bg);
 }
 
 /**
- * Draws a text at given position. Optionally wraps at a maximum length. Currently does not work with hex layout.
+ * Draws a text at given position. Optionally wraps at a maximum length. Currently does not work with hex layout. This applies to the background layer.
  * @param {int} x
  * @param {int} y
  * @param {string} text May contain color/background format specifiers, %c{name}/%b{name}, both optional. %c{}/%b{} resets to default.
@@ -162,41 +169,7 @@ ROT.Display.prototype.draw = function(x, y, ch, fg, bg) {
  * @returns {int} lines drawn
  */
 ROT.Display.prototype.drawText = function(x, y, text, maxWidth) {
-	var fg = null;
-	var bg = null;
-	var cx = x;
-	var cy = y;
-	var lines = 1;
-
-	var tokens = ROT.Text.tokenize(text, maxWidth);
-
-	while (tokens.length) { /* interpret tokenized opcode stream */
-		var token = tokens.shift();
-		switch (token.type) {
-			case ROT.Text.TYPE_TEXT:
-				for (var i=0;i<token.value.length;i++) {
-					this.draw(cx++, cy, token.value.charAt(i), fg, bg);
-				}
-			break;
-
-			case ROT.Text.TYPE_FG:
-				fg = token.value || null;
-			break;
-
-			case ROT.Text.TYPE_BG:
-				bg = token.value || null;
-			break;
-
-			case ROT.Text.TYPE_NEWLINE:
-				cx = x;
-				cy++;
-				lines++
-			break;
-
-		}
-	}
-
-	return lines;
+	this._background.drawText(x, y, text, maxWidth);
 }
 
 /**
@@ -213,19 +186,45 @@ ROT.Display.prototype.measureText = function(text, maxWidth) {
  * Timer tick: update dirty parts
  */
 ROT.Display.prototype._tick = function() {
-	if (!this._dirty) { return; }
 
-	if (this._dirty === true) { /* draw all */
+	if (this._dirty === true) {
 		this._context.fillStyle = this._options.bg;
 		this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
-
-		for (var id in this._data) { /* redraw cached data */
-			this._draw(id, false);
+	}
+	
+	for (var i=0; i < this._layers.length; i++) { 
+		var layer = this._layers[i];
+		if (this._dirty === true || layer._dirty === true) {
+			// re-paint the whole layer
+						
+			for (var id in layer._data) { /* redraw cached data */
+				this._draw(layer._data[id], true);
+			}
 		}
-
-	} else { /* draw only dirty */
-		for (var key in this._dirty) {
-			this._draw(key, true);
+		else if (layer._dirty) {
+			// incremental update
+			for (var key in layer._dirty) {
+				var dirtyState = layer._dirty[key];
+				var x = dirtyState[0];
+				var y = dirtyState[1];
+				
+				switch (this._options.layout) {
+					case "rect":
+						var cx = (x+0.5) * this._spacingX;
+						var cy = (y+0.5) * this._spacingY;
+						
+						this._context.fillStyle = this._options.bg;
+						this._context.fillRect(cx-this._spacingX/2, cy-this._spacingY/2, this._spacingX, this._spacingY);
+					break;
+					case "hex":
+						var cx = (x+1) * this._spacingX;
+						var cy = y * this._spacingY + this._hexSize;
+						this._context.fillStyle = this._option.bg;
+						this._fillHex(cx, cy);
+					break;
+				}
+			}
+			layer._dirty = false;
 		}
 	}
 
@@ -236,8 +235,7 @@ ROT.Display.prototype._tick = function() {
  * @param {string} key What to draw
  * @param {bool} clearBefore Is it necessary to clean before?
  */
-ROT.Display.prototype._draw = function(key, clearBefore) {
-	var data = this._data[key];
+ROT.Display.prototype._draw = function(data, clearBefore) {
 	var x = data[0];
 	var y = data[1];
 	var ch = data[2];
@@ -312,6 +310,122 @@ ROT.Display.prototype._compute = function() {
 	this._context.font = font;
 	this._context.textAlign = "center";
 	this._context.textBaseline = "middle";
+}
+
+/**
+ * @class Layer which may be drawn to.
+ */
+ROT.Layer = function(display) {
+	this._data = {};
+	this._dirty = false; /* false = nothing, true = all, object = dirty cells */
+	this._display = display;
+}
+
+/**
+ * Clear the whole layer (cover it with background color)
+ */
+ROT.Layer.prototype.clear = function() {
+	this._data = {};
+	this._dirty = true;
+}
+
+/**
+ * Clear a specified region of the layer.
+ * 
+ * @param {int} x
+ * @param {int} y
+ * @param {int} w
+ * @param {int} h
+ */
+ROT.Layer.prototype.clearRect = function(x, y, w, h) {
+	for (var dx=x; dx <= (x + w); dx++) {
+		for (var dy=y; dy <= (y + h); dy++) {
+			delete this._data[dx+","+dy];
+			if (!this._dirty) { this._dirty = {}; } /* first! */
+			this._dirty[dx+","+dy] = [dx, dy];
+		}
+	}
+}
+
+/**
+ * @param {int} x
+ * @param {int} y
+ * @param {string} ch 
+ * @param {string} [fg] foreground color
+ * @param {string} [bg] background color
+ */
+ROT.Layer.prototype.draw = function(x, y, ch, fg, bg) {
+	if (!fg) { fg = this._display._options.fg; }
+	if (!bg) { bg = this._display._options.bg; }
+	this._data[x+","+y] = [x, y, ch, fg, bg];
+	
+	if (this._dirty === true) { return; } /* will already redraw everything */
+	if (!this._dirty) { this._dirty = {}; } /* first! */
+	this._dirty[x+","+y] = true;
+}
+
+/**
+ * Draws a text at given position. Optionally wraps at a maximum length. Currently does not work with hex layout.
+ * @param {int} x
+ * @param {int} y
+ * @param {string} text May contain color/background format specifiers, %c{name}/%b{name}, both optional. %c{}/%b{} resets to default.
+ * @param {int} [maxWidth] wrap at what width?
+ * @returns {int} lines drawn
+ */
+ROT.Layer.prototype.drawText = function(x, y, text, maxWidth) {
+	var fg = null;
+	var bg = null;
+	var cx = x;
+	var cy = y;
+	var lines = 1;
+
+	var tokens = ROT.Text.tokenize(text, maxWidth);
+
+	while (tokens.length) { /* interpret tokenized opcode stream */
+		var token = tokens.shift();
+		switch (token.type) {
+			case ROT.Text.TYPE_TEXT:
+				for (var i=0;i<token.value.length;i++) {
+					this.draw(cx++, cy, token.value.charAt(i), fg, bg);
+				}
+			break;
+
+			case ROT.Text.TYPE_FG:
+				fg = token.value || null;
+			break;
+
+			case ROT.Text.TYPE_BG:
+				bg = token.value || null;
+			break;
+
+			case ROT.Text.TYPE_NEWLINE:
+				cx = x;
+				cy++;
+				lines++
+			break;
+
+		}
+	}
+
+	return lines;
+}
+
+/**
+ * Delete this layer.
+ */
+ROT.Layer.prototype.remove = function() {
+	var index = display._layers.indexOf(item);
+	display._layers.splice(index, 1);
+}
+
+/**
+ * Create a new layer.
+ * @returns {ROT.Layer} layer object
+ */
+ROT.Display.prototype.newLayer = function() {
+	var layer = new ROT.Layer(this);
+	this._layers.push(layer);
+	return layer;
 }
 /**
  * @namespace
