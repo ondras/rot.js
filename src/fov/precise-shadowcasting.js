fb5af5fb43b5135c47e4ece8b5ff7ba85f18695c
Ondrej Zara <ondrej.zara@gmail.com>
2013-01-03 10:24:30 +0100

    experiment with simplified _visibleCoords

--- src/fov/precise-shadowcasting.js
+++ src/fov/precise-shadowcasting.js
@@ -1,5 +1,4 @@
 var L = function() { 
-	return;
 	var args = [];
 	for (var i=0;i<arguments.length;i++) {
 		var a = arguments[i];
@@ -30,7 +29,7 @@ ROT.FOV.PreciseShadowcasting.prototype.compute = function(x, y, R, callback) {
 	/* list of all shadows */
 	var SHADOWS = [];
 	
-	var cx, cy, blocks, arc;
+	var cx, cy, blocks, A1, A2;
 
 	/* analyze surrounding cells in concentric rings, starting from the center */
 	for (var r=1; r<=R; r++) {
@@ -41,88 +40,79 @@ ROT.FOV.PreciseShadowcasting.prototype.compute = function(x, y, R, callback) {
 		for (var i=0;i<neighborCount;i++) {
 			cx = neighbors[i][0];
 			cy = neighbors[i][1];
-			arc = [i ? 2*i-1 : 2*neighborCount-1, 2*neighborCount, 2*i+1, 2*neighborCount]; /* shift half-an-angle backwards to maintain consistency of 0-th cells */
-			L("new arc", arc);
+			/* shift half-an-angle backwards to maintain consistency of 0-th cells */
+			A1 = [i ? 2*i-1 : 2*neighborCount-1, 2*neighborCount];
+			A2 = [2*i+1, 2*neighborCount]; 
+			L("new arc", A1, A2);
 			
 			blocks = !this._lightPasses(cx, cy);
-			if (this._visibleCoords(arc, blocks, SHADOWS)) { callback(cx, cy, r); }
+			if (this._visibleCoords(A1, A2, blocks, SHADOWS)) { callback(cx, cy, r); }
+
 			L("current shadows:", SHADOWS);
-			if (SHADOWS.length == 1 && SHADOWS[0][0] == 0 && SHADOWS[0][2] == SHADOWS[0][3]) { L("cutoff at", SHADOWS); return; } /* cutoff? */
+			if (SHADOWS.length == 2 && SHADOWS[0][0] == 0 && SHADOWS[1][0] == SHADOWS[1][1]) { L("cutoff at", SHADOWS); return; } /* cutoff? */
 
 		} /* for all cells in this ring */
 	} /* for all rings */
 }
 
 /**
- * @param {int[4]} arc
+ * @param {int[2]} A1 arc start
+ * @param {int[2]} A2 arc end
  * @param {bool} blocks Does current cell block visibility?
  * @param {int[][]} SHADOWS list of active shadows
  */
-ROT.FOV.PreciseShadowcasting.prototype._visibleCoords = function(arc, blocks, SHADOWS) {
-	L("checking arc", arc, "whose blocking is", blocks);
-	if (arc[0] > arc[2]) {
+ROT.FOV.PreciseShadowcasting.prototype._visibleCoords = function(A1, A2, blocks, SHADOWS) {
+	L("checking arc", A1, A2, "whose blocking is", blocks);
+
+	if (A1[0] > A2[0]) {
 		L("zero encountered - splitting into two");
-		var v1 = arguments.callee([arc[0], arc[1], arc[1], arc[1]], blocks, SHADOWS);
-		var v2 = arguments.callee([0, 1, arc[2], arc[3]], blocks, SHADOWS);
+		var v1 = arguments.callee(A1, [A1[1], A1[1]], blocks, SHADOWS);
+		var v2 = arguments.callee([0, 1], A2, blocks, SHADOWS);
 		return (v1 || v2);
 	}
 
-	var old, index = 0;
+	/* find indices of shadows which are >= our new arc */
+	var index1 = -1, index2 = -1, index = 0;
 	while (index < SHADOWS.length) {
-		old = SHADOWS[index++];
-		/* note that the old shadow is always >= new arc. this is important, as it eliminates certain problematic cases */
+		var old = SHADOWS[index];
+		if (index1 == -1 && old[0]*A1[1] >= A1[0]*old[1]) { index1 = index; }
+		if (index2 == -1 && old[0]*A2[1] > A2[0]*old[1]) { index2 = index; break; } /* important! we check GREATER (not greater-or-equal) for the second point! */
+		index++;
+	}
 
-		if (old[0]*arc[3] > arc[2]*old[1]) { /* old shadow starts completely after this one */
-			L("inserting before old shadow", old);
-			if (blocks) { SHADOWS.splice(index-1, 0, arc); }
-			return true;
+	L("index1", index1);
+	L("index2", index2);
+	if (index2 == -1) { index2 = SHADOWS.length; }
+	if (index1 == -1) { index1 = SHADOWS.length; }
 
-		} else if (old[0]*arc[1] >= arc[0]*old[1]) { /* old shadow starts within this arc */
+	if (index2 % 2) { /* we end WITHIN an existing shadow */
+		var remove = index2-index1; /* we will remove this many shadows */
+		if (!remove) { return false; } /* subset of an existing shadow */
 
-			/* there is an overlap:
-			 * 1) this arc and old shadow share the starting point
-			 * 2) old shadow starts in this one
-			 */
-
-			if (old[0]*arc[1] == arc[0]*old[1]) { L("completely obstructed by", old); return false; } /* case #1, shared first edge, old is >= this one => consumed, not visible */
-
-			/* case #2, old shadow starts inside of this arc, enlarge it */
-			if (blocks) {
-				L("enlarging old", old);
-				old[0] = arc[0];
-				old[1] = arc[1];
+		if (blocks) { /* adjust */
+			if (remove % 2) { /* remove and insert */
+				SHADOWS.splice(index1, remove, A1);
+			} else { /* just remove */
+				SHADOWS.splice(index1, remove);
 			}
-			return true;
-
-		} else if (old[2]*arc[1] >= arc[0]*old[3]) { /* old shadow ends after or within this arc */
-
-			/* there is an overlap:
-			 * 1) old shadow completely overlaps this one
-			 * 2) old shadow will merge with this one
-			 * 3) this one will merge two older shadows
-			 */
-
-			if (old[2]*arc[3] >= arc[2]*old[3]) { L("completely overlapped with previous shadow", old); return false; } /* case #1, completely overlapped */
-
-			if (!blocks) { return true; }
-
-			var next = SHADOWS[index];
-			if (next && (next[0]*arc[3] <= arc[2]*next[1])) { /* case #3, merge two old shadows */
-				L("merging", old, next);
-				old[2] = next[2];
-				old[3] = next[3];
-				SHADOWS.splice(index, 1);
-			} else { /* case #2, adjust old shadow */
-				L("enlarging old", old);
-				old[2] = arc[2];
-				old[3] = arc[3];
-			}
-			return true;
 		}
 
+		return true;
+
+	} else { /* we end OUTSIDE of an existing shadow */
+
+		if (blocks) { /* adjust */
+			var remove = index2-index1;
+			if (remove % 2) { /* remove and insert second */
+				SHADOWS.splice(index1, remove, A2);
+			} else { /* remove and insert both (append when remove=0) */
+				SHADOWS.splice(index1, remove, A1, A2);
+			}
+		}
+
+		/* check for special case when the new arc is a subset of an existing, sharing the second edge */
+		
+
+		return true;
 	}
-	
-	/* this is a new shadow, add it to our list */
-	if (blocks) { L("appending as a new shadow"); SHADOWS.push(arc); }
-	return true;
 }
