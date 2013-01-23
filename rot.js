c5f877ba634692c709e5b661b69c970cd91a8e9d
Ondrej Zara <ondrej.zara@gmail.com>
2013-01-23 16:33:12 +0100

    colors

--- rot.js
+++ rot.js
@@ -1,6 +1,6 @@
 /*
 	This is rot.js, the ROguelike Toolkit in JavaScript.
-	Version 0.3~dev, generated on Wed Jan 23 16:18:07 CET 2013.
+	Version 0.3~dev, generated on Wed Jan 23 16:27:29 CET 2013.
 */
 
 /**
@@ -2841,25 +2841,68 @@ ROT.Color = {
 	fromString: function(str) {
 
 	},
+
+	/**
+	 * Add two or more colors
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @returns {number[]}
+	 */
 	add: function(color1, color2) {
-
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			for (var j=1;j<arguments.length;j++) {
+				result[i] += arguments[j][i];
+			}
+		}
+		return result;
 	},
+
+	/**
+	 * Multiply (mix) two or more colors
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @returns {number[]}
+	 */
 	multiply: function(color1, color2) {
-
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			for (var j=1;j<arguments.length;j++) {
+				result[i] *= arguments[j][i] / 255;
+			}
+			result[i] = Math.round(result[i]);
+		}
+		return result;
 	},
-	mix: function(color1, color2, factor) {
 
+	/**
+	 * Interpolate (blend) two colors with a given factor
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @param {float} factor 0..1
+	 * @returns {number[]}
+	 */
+	interpolate: function(color1, color2, factor) {
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			result[i] = Math.round(result[i] + factor*(color2[i]-color1[i]));
+		}
+		return result;
 	},
+
 	randomize: function(color, diff) {
 
 	},
+
 	toRGB: function(color) {
-		return "rgb" + (color.length > 3 ? "a" : "") + "(" + color.join(",") + ")";
+		return "rgb(" + color.join(",") + ")";
 	},
+
 	toHex: function(color) {
 		
 	}
-}/**
+}
+/**
  * @class Lighting computation, based on a traditional FOV for multiple light sources and multiple passes.
  * @param {function} reflectivityCallback Callback to retrieve cell reflectivity (0..1)
  * @param {object} [options]
--- src/color.js
+++ src/color.js
@@ -5,21 +5,63 @@ ROT.Color = {
 	fromString: function(str) {
 
 	},
+
+	/**
+	 * Add two or more colors
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @returns {number[]}
+	 */
 	add: function(color1, color2) {
-
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			for (var j=1;j<arguments.length;j++) {
+				result[i] += arguments[j][i];
+			}
+		}
+		return result;
 	},
+
+	/**
+	 * Multiply (mix) two or more colors
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @returns {number[]}
+	 */
 	multiply: function(color1, color2) {
-
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			for (var j=1;j<arguments.length;j++) {
+				result[i] *= arguments[j][i] / 255;
+			}
+			result[i] = Math.round(result[i]);
+		}
+		return result;
 	},
-	mix: function(color1, color2, factor) {
 
+	/**
+	 * Interpolate (blend) two colors with a given factor
+	 * @param {number[]} color1
+	 * @param {number[]} color2
+	 * @param {float} factor 0..1
+	 * @returns {number[]}
+	 */
+	interpolate: function(color1, color2, factor) {
+		var result = color1.clone();
+		for (var i=0;i<3;i++) {
+			result[i] = Math.round(result[i] + factor*(color2[i]-color1[i]));
+		}
+		return result;
 	},
+
 	randomize: function(color, diff) {
 
 	},
+
 	toRGB: function(color) {
-		return "rgb" + (color.length > 3 ? "a" : "") + "(" + color.join(",") + ")";
+		return "rgb(" + color.clamp(0, 255).join(",") + ")";
 	},
+
 	toHex: function(color) {
 		
 	}
